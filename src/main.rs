e492a91b099cc84c3c46eea661b7be6d3fd4692a
Maxwell L-T <maxwell.lt@live.com>
2020-08-28 17:24:16 -0400

    Directory listing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::path::Path;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(".");
+    let files = path.read_dir().expect("Could not read directory contents");
+    for file in files {
+      match file {
+        Err(_) => println!("Error in directory listing"),
+        Ok(f) => match (*f.path()).is_file() {
+          true => println!("{}", f.file_name().into_string().expect("").cyan()),
+          false => println!("{}", f.file_name().into_string().expect("").blue()),
+        }
+      }
+    }
 }
