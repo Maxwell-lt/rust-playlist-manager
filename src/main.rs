189ec75fda1195843fbc3ac006db0736241fe09b
Maxwell L-T <maxwell.lt@live.com>
2020-08-28 17:53:39 -0400

    Filter files

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@ use std::path::Path;
 use colored::*;
 
 fn main() {
-    let path = Path::new(".");
-    let files = path.read_dir().expect("Could not read directory contents");
-    for file in files {
-      match file {
-        Err(_) => println!("Error in directory listing"),
-        Ok(f) => match (*f.path()).is_file() {
-          true => println!("{}", f.file_name().into_string().expect("").cyan()),
-          false => println!("{}", f.file_name().into_string().expect("").blue()),
-        }
-      }
+  let dir_list = Path::new(".")
+                  .read_dir().expect("Could not read directory contents");
+  let files = dir_list.filter_map(|file| 
+  {
+    match file {
+      Ok(f) =>
+        match f.path().is_file() {
+          true => Some(f.path()),
+          false => None,
+        },
+      Err(_) => None,                     
     }
+  });
+  for file in files {
+    println!("{}",
+      file.to_str().expect("Invalid string").blue());
+  }
 }
