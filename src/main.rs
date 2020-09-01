203fc5514fec18a771e9f48272cc2277d13d4b3d
Maxwell L-T <maxwell.lt@live.com>
2020-09-01 16:53:34 -0400

    Show file extensions
    Took me forever to figure out how to deal with the lifetimes for this

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use std::path::Path;
+use std::fs::ReadDir;
+use std::fs::DirEntry;
 use colored::*;
 
+fn get_file_list() -> ReadDir {
+  Path::new(".").read_dir().expect("Could not read directory contents")
+}
+
+fn get_file_extension(file: DirEntry) -> Option<String> {
+  let path = file.path();
+  match path.extension() {
+    Some(e) => e.to_str().map(String::from),
+    None => None
+  }
+}
+
 fn main() {
-  let dir_list = Path::new(".")
-                  .read_dir().expect("Could not read directory contents");
-  let files = dir_list.filter_map(|file| 
-  {
-    match file {
-      Ok(f) =>
-        match f.path().is_file() {
-          true => Some(f.path()),
-          false => None,
-        },
-      Err(_) => None,                     
+  for file_result in get_file_list() {
+    let file = file_result.expect("Invalid file");
+    println!("File: {}", file.file_name().to_str().expect("Invalid string").blue());
+    let extension = get_file_extension(file);
+    match extension {
+      Some(e) => println!("Extension: {}", e.blue()),
+      _ => println!("{}", "No extension".red()),
     }
-  });
-  for file in files {
-    println!("{}",
-      file.to_str().expect("Invalid string").blue());
+    println!("");
   }
 }
