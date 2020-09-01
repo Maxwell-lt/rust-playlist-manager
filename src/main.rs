ce49f2e78929f3d8dcb498250c62d391fa1928b3
Maxwell L-T <maxwell.lt@live.com>
2020-09-01 19:39:46 -0400

    Print list of m3u files in current directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::path::PathBuf;
 use std::fs::ReadDir;
 use std::fs::DirEntry;
 use colored::*;
@@ -7,7 +8,7 @@ fn get_file_list() -> ReadDir {
   Path::new(".").read_dir().expect("Could not read directory contents")
 }
 
-fn get_file_extension(file: DirEntry) -> Option<String> {
+fn get_file_extension(file: &DirEntry) -> Option<String> {
   let path = file.path();
   match path.extension() {
     Some(e) => e.to_str().map(String::from),
@@ -15,15 +16,31 @@ fn get_file_extension(file: DirEntry) -> Option<String> {
   }
 }
 
-fn main() {
-  for file_result in get_file_list() {
-    let file = file_result.expect("Invalid file");
-    println!("File: {}", file.file_name().to_str().expect("Invalid string").blue());
-    let extension = get_file_extension(file);
-    match extension {
-      Some(e) => println!("Extension: {}", e.blue()),
-      _ => println!("{}", "No extension".red()),
+fn get_m3u_files(files: ReadDir) -> Vec<PathBuf> {
+  let mut m3u_files: Vec<PathBuf> = Vec::new();
+  for file in files {
+    if let Ok(f) = file {
+      if let Some(ext) = get_file_extension(&f) {
+        if ext == "m3u" {
+          m3u_files.push(f.path());
+        }
+      }
     }
-    println!("");
+  }
+  m3u_files
+}
+
+fn get_file_name(path: PathBuf) -> Option<String> {
+  path.file_name()
+    .and_then(|f| f.to_str())
+    .map(String::from)
+}
+
+fn main() {
+  let playlists = get_m3u_files(get_file_list());
+  for pl in playlists.into_iter().map(get_file_name).collect::<Vec<_>>() {
+    if let Some(filename) = pl {
+      println!("{}", filename.blue());
+    }
   }
 }
